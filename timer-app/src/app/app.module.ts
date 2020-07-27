032a2b3a33664b80057461d832ae97ddf4085ef9
Tetiana Dudarenko <tanya.dudarenko@gmail.com>
2020-07-27 21:58:59 +0300

    Added styles and set time btn

--- timer-app/src/app/app.module.ts
+++ timer-app/src/app/app.module.ts
@@ -4,6 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +22,13 @@ import { TimerComponent } from './timer/timer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- timer-app/src/app/timer/timer.component.ts
+++ timer-app/src/app/timer/timer.component.ts
@@ -7,9 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  public timer: Timer;
+  private timerStoped: boolean;
+  private isTimerSet: boolean;
   constructor() { }
 
-  ngOnInit(): void {
+  public IsTimerSet(): boolean
+  {
+      return this.isTimerSet;
   }
 
+  ngOnInit(): void {
+    this.timer = new Timer();
+    this.isTimerSet = false;
+  }
+
+  async onTimerStart() {
+    this.timerStoped = false;
+    this.caculateTime();
+  }
+
+  async caculateTime()
+  {
+    while(this.timer.Minutes > 0 && !this.timerStoped)
+    {
+      await this.sleep(1000);
+
+      if(this.timer.Seconds > 0)
+      {
+        this.timer.Seconds -= 1;
+      }
+      else
+      {
+        this.timer.Minutes -= 1;
+      }
+    }
+  }
+
+  sleep(ms)
+  {
+    return new Promise((resolve) => {setTimeout(resolve, ms);});
+  }
+
+  onTimerStop()
+  {
+      this.timerStoped = true;
+  }
+
+  onTimerReset()
+  {
+    this.timerStoped = false;
+    console.log("Timer is reset");
+  }
+
+  onTimeSetSubmit()
+  {
+    this.isTimerSet = true;
+  }
+
+  onTimerSet()
+  {
+    this.isTimerSet = false;
+  }
+
+}
+
+export class Timer{
+  public Minutes: number;
+  public Seconds: number;
+
+  constructor(minutes: number = 0, seconds: number = 0){
+    this.Minutes = minutes;
+    this.Seconds = seconds;
+  }
 }
